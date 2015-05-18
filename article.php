7812634be23951004bafa04d41f4a9a57fa16ca9
Demosth3nes <jermaine911@hotmail.com>
2015-05-18 22:55:26 +0100

    removing content

--- article.php
+++ article.php
@@ -19,49 +19,54 @@
 var app = angular.module('app',[]);
 
 app.controller('MainCtrl',function($scope,$compile){
-$scope.count = 0;
+$scope.count = 1;
 $scope.order = [];
-var idNumber = "id" + $scope.count;
+
+// var idNumber = "id" + $scope.count;
 $scope.remove = function(){
-	CKEDITOR.instances.idNumber.destroy();
-	var element = CKEDITOR.document.getById( idNumber );
-						element.remove();
 	
+	// CKEDITOR.instances.idNumber.destroy();
+	// var element = CKEDITOR.document.getById( idNumber );
+	// 					element.remove();
+
 
 
 	}
 });
-	// app.directive('namer',function(){
-	// 	return{
-	// 		scope:true,
-	// 		restrict:'C', //Look for an element
-	// 		link:function(scope,element,attribute){
-	// 			scope.fullName = attribute.first + ' ' + attribute.last
-	// 		},
-	// 		replace:true,
-	// 		template:'<div class = "simple"><h1>Hey there man this a good test</h1></div>' //Can point to a template URL
-	// 	}
-	// })
-
 	app.directive('additem', function($compile){
     	return{ 
 	    	link:function(scope,element,attrs){
-
+	    		
 	    		element.bind("click",function(){
-
-	    			var idNumber = "id" + (scope.count ++);
+	    			scope.idNumber = "id" + scope.count ++;
+	    			
 	    			angular.element(document.getElementById('example'))
-	    				.append($compile('<div id = "'+idNumber+'" name = "'+idNumber+'"  contenteditable = "true" class  = "namer">Hi there</div>')(scope))
-	    				.append($compile('<button ng-click = "remove()" class = "remove">Remove item</button>')(scope));
-
-	    				CKEDITOR.inline( idNumber );
+	    				.append($compile('<div id = "'+scope.idNumber+'" name = "'+scope.idNumber+'"  contenteditable = "true" class  = "namer">Hi there</div>')(scope))
+	    				.append($compile('<button itemid = "'+scope.idNumber+'" data-removeitem ng-click = "remove()" class = "remove">Remove item</button>')(scope));
+	    			
+	    				CKEDITOR.inline( scope.idNumber );
 	    				
 	    		});
 	    	}
      	};
     });
- 	
 
+    app.directive('removeitem', function($compile){
+    	return{
+	    	link:function(scope,element,attrs){
+
+	    		element.bind("click",function(){
+    			console.log(attrs.itemid);
+    			var ck = CKEDITOR.document.getById( attrs.itemid );
+				ck.remove();
+
+				element.remove();
+
+    			});
+	    	}
+     	};
+    });
+ 	
 </script>
 
 </html>
